a2e7aec15e834342db6595aee6bfb57a2339e0ae
Riccardo Di Benedetto <dibe.ricky@gmail.com>
2022-11-10 21:18:56 +0100

    fix

--- migrations/2022-11-10-193355_create_events/up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE events (
-  id SERIAL PRIMARY KEY,
-  payload VARCHAR NOT NULL,
-  event_type VARCHAR NOT NULL,
-  aggregate_id VARCHAR NOT NULL,
-  created_at TIMESTAMPTZ DEFAULT Now() 
-)
