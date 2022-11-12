146caa08d7baf8d813991d974b09662058082a57
Riccardo Di Benedetto <dibe.ricky@gmail.com>
2022-11-12 17:23:33 +0100

    chenged

--- members/web/src/main.rs
+++ members/web/src/main.rs
@@ -3,13 +3,13 @@ use repository::repo::Repository;
 
 fn main() -> Result<()> {
     let mut repo = Repository::default();
-    // controller::add_tweet(&mut repo, "@dibericky",  "Hi there!")?;
+    controller::add_tweet(&mut repo, "@dibericky",  "Hi there! Check file members/web/src/main.rs")?;
 
-    controller::edit_tweet(
-        &mut repo,
-        "b89e6b1b-21fd-49bb-8a0c-3cd697b81142",
-        "This is another changed with mb!",
-    )?;
+    // controller::edit_tweet(
+    //     &mut repo,
+    //     "b89e6b1b-21fd-49bb-8a0c-3cd697b81142",
+    //     "This is another changed with mb!",
+    // )?;
 
     Ok(())
 }
